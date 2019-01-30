341bcee88a74b96a01ce06488fe5872057fb3c91
Noraz <Noraz@DESKTOP-J9DQDUS>
2019-01-30 20:47:23 +0100

    Add project files.

---
+++ cw/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for (int n = 3; n <= 20; n++)
+            {
+                if (czy_pierwsza(n))
+                {
+                    Console.WriteLine("Liczba: "+n+" jest pierwsza");
+                    Console.ReadKey();
+
+
+                }
+                else
+                    Console.WriteLine("Liczba: "+n+" nie jest pierwsza");
+                Console.ReadKey();
+
+            }
+            bool czy_pierwsza(int n)
+            {
+                if (n < 2)
+                {
+                    Console.WriteLine("gdy liczba jest mniejsza niż 2 to nie jest pierwszą");
+                    Console.ReadKey();
+
+                    return false;
+                }
+                for (int i = 2; i * i < n; i++)
+                {
+                    if (n % i == 0)
+                        Console.WriteLine("gdy znajdziemy dzielnik, to dana liczba nie jest pierwsza");
+                    Console.ReadKey();
+
+                    return false;
+                }
+                return true;
+            }
+
+        }
+           
+        
+    }
+    
+}
