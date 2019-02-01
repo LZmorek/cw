abfbed1f2856d061998c6c290ce96beccb6a8899
Noraz69 <41554263+Noraz69@users.noreply.github.com>
2019-02-01 13:49:41 +0100

    dziaaalaaaa

--- cw/Program.cs
+++ cw/Program.cs
@@ -10,38 +10,34 @@ namespace cw
     {
         static void Main(string[] args)
         {
-            
-
-            for (int n = 3; n <= 20; n++)
+            for (int n = 2; n <= 20; n++)
             {
                 if (czy_pierwsza(n))
                 {
                     Console.WriteLine("Liczba: "+n+" jest pierwsza");
                     Console.ReadKey();
-
-
                 }
                 else
-                    Console.WriteLine("Liczba: "+n+" nie jest pierwsza");
+                Console.WriteLine("Liczba: "+n+" nie jest pierwsza");
                 Console.ReadKey();
-
             }
             bool czy_pierwsza(int n)
             {
+                
                 if (n < 2)
                 {
                     Console.WriteLine("gdy liczba jest mniejsza niż 2 to nie jest pierwszą");
                     Console.ReadKey();
-
                     return false;
                 }
-                for (int i = 2; i * i < n; i++)
+                for (int i = 2;  i < n; i++)
                 {
                     if (n % i == 0)
+                    {
                         Console.WriteLine("gdy znajdziemy dzielnik, to dana liczba nie jest pierwsza");
-                    Console.ReadKey();
-
-                    return false;
+                        Console.ReadKey();
+                        return false;
+                    }
                 }
                 return true;
             }
